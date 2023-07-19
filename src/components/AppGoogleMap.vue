5f065cd663076acb2015468825e2ae6989e82b23
SamuelPChao <samchaoss@gmail.com>
2023-07-19 13:42:06 +0800

    google map template modified

--- src/components/AppGoogleMap.vue
+++ src/components/AppGoogleMap.vue
@@ -1,29 +1,27 @@
 <script>
-import { Loader } from '@googlemaps/js-api-loader'
+import { Loader } from "@googlemaps/js-api-loader";
 export default {
-  name: 'AppGoogleMap.vue',
+  name: "AppGoogleMap.vue",
   created() {
     const loader = new Loader({
-      apiKey: 'AIzaSyBTxkvRAY4d3bYuTcJf3Lh1PthikJ-5UWc',
-      version: 'weekly'
-    })
+      apiKey: "AIzaSyBTxkvRAY4d3bYuTcJf3Lh1PthikJ-5UWc",
+      version: "weekly",
+    });
     loader.load().then(async () => {
-      const myLatLng = { lat: 25.049, lng: 121.514 }
-      const { Map } = await google.maps.importLibrary(
-        'maps'
-      )
-      const map = new Map(document.getElementById('map'), {
+      const myLatLng = { lat: 25.049, lng: 121.514 };
+      const { Map } = await google.maps.importLibrary("maps");
+      const map = new Map(document.getElementById("map"), {
         center: { lat: 25.049, lng: 121.514 },
-        zoom: 20
-      })
+        zoom: 20,
+      });
       new google.maps.Marker({
         position: myLatLng,
         map,
-        title: 'Hello World!'
-      })
-    })
-  }
-}
+        title: "Hello World!",
+      });
+    });
+  },
+};
 </script>
 
 <template>
@@ -32,7 +30,7 @@ export default {
 
 <style lang="scss" scoped>
 #map{
-  width: 80vw;
+  width: 80%;
   height: 27rem;
   margin:0 auto;
 }
